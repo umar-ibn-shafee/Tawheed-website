8e2fdf3f20ddb84da8a686fb8d56c9696f778460
Mohammed Umar Ibn Shafee <umaribnshafee@Mohammeds-MacBook-Pro.local>
2022-07-01 23:38:17 +0530

    created layout components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
+import Footer from '../components/footer'
 
 const Home: NextPage = () => {
   return (
@@ -26,14 +27,7 @@ const Home: NextPage = () => {
           </Link>
         </p>
       </main>
-      <footer className='h-80 px-12 py-4 absolute inset-x-0 bottom-0 bg-black'>
-          <div className='p-4 w-max'>
-            <Image src="/logo-horizontal.png" alt="Masjid-e-Tawheed Logo" width={250} height={100} />
-          </div>
-          <div className='absolute inset-x-0 bottom-0 py-2 text-white text-xs text-center'>
-            <p>Copyright © 2022 Masjid-e-Tawheed. All rights reserved.</p>
-          </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
