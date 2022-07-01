295c95f2cc0ff36cc9a913d63ed79cd6f5c0a60c
umar-ibn-shafee <md.umar05@gmail.com>
2022-07-01 23:52:28 +0530

    added components for layout

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+const footer = () => {
+    return (
+        <div>
+            <footer className='h-80 px-12 py-4 absolute inset-x-0 bottom-0 bg-black'>
+                <div className='p-4 w-max'>
+                    <Image src="/logo-horizontal.png" alt="Masjid-e-Tawheed Logo" width={250} height={100} />
+                </div>
+                <div className='absolute inset-x-0 bottom-0 py-2 bg-gradient-to-b from-gray-500 to-black text-white text-xs text-center'>
+                    <p>Copyright © 2022 Masjid-e-Tawheed. All rights reserved.</p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default footer;
