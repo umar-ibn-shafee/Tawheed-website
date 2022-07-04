c6a45c22195b7a8b992e47b90cbf6a0bbd357943
umar-ibn-shafee <md.umar05@gmail.com>
2022-07-04 23:36:52 +0530

    css fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,12 @@ const footer = () => {
     return (
         <div className='relative'>
             <footer className='h-80 px-12 py-4 bg-black'>
-                <div className='p-4 w-max'>
-                    <Image src="/logo-horizontal.png" alt="Masjid-e-Tawheed Logo" width={250} height={100} />
+                <div className='grid grid-row-5 gap-4 h-full'>
+                    <div className='row-end-6 row-span-1 p-4 w-max'>
+                        <Image src="/logo-horizontal.png" alt="Masjid-e-Tawheed Logo" width={250} height={100} />
+                    </div>
                 </div>
-                <div className='absolute inset-x-0 bottom-0 py-2 bg-gradient-to-b from-gray-500 to-black text-white text-xs text-center'>
+                <div className='absolute inset-x-0 bottom-0 py-2 bg-gradient-to-b from-gray-800 to-black text-white text-xs text-center'>
                     <p>Copyright © 2022 Masjid-e-Tawheed. All rights reserved.</p>
                 </div>
             </footer>
