b5f911b68506d3ee01485560de6767ab66e6395c
umar-ibn-shafee <md.umar05@gmail.com>
2022-07-09 12:25:51 +0530

    header clean code and color themed tailwind

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,24 +2,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Header = () => {
+    const styles = {
+        mainBlock: "bg-primary px-32 py-6 sticky top-0 w-full flex justify-between items-center",
+        subBlock1: "w-max p-2",
+        subBlock2: "flex space-x-10 text-base font-semibold",
+        navLinks: "hover:underline underline-offset-2 hover:text-secondary duration-300 rounded-lg drop-shadow-xl decoration-secondary decoration-4 p-2"
+    }
     return (
-        <nav className="bg-[#f9f7f2] px-32 py-6 sticky top-0 w-full flex justify-between items-center">
-            <div className='w-max p-2'>
+        <nav className={styles.mainBlock}>
+            <div className={styles.subBlock1}>
                 <Link href='/'>
                     <a>
                         <Image src="/logo-horizontal.png" alt="Masjid-e-Tawheed Logo" width={190} height={70} ></Image>
                     </a>
                 </Link>
             </div>
-            <div className='flex space-x-10 text-base font-semibold'>
+            <div className={styles.subBlock2}>
                 <Link href='/services'>
-                    <a className='hover:underline underline-offset-2 hover:text-emerald-600 duration-300 rounded-lg drop-shadow-xl decoration-emerald-600 decoration-4 p-2'>Services</a>
+                    <a className={styles.navLinks}>Services</a>
                 </Link>
                 <Link href='/about-us'>
-                    <a className='hover:underline underline-offset-2 hover:text-emerald-600 duration-300 rounded-lg drop-shadow-xl decoration-emerald-600 decoration-4 p-2'>About Us</a>
+                    <a className={styles.navLinks}>About Us</a>
                 </Link>
                 <Link href='/contact-us'>
-                    <a className='hover:underline underline-offset-2 hover:text-emerald-600 duration-300 rounded-lg drop-shadow-xl decoration-emerald-600 decoration-4 p-2'>Contact Us</a>
+                    <a className={styles.navLinks}>Contact Us</a>
                 </Link>
             </div>
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["src/pages/**/*.{js,ts,jsx,tsx}", "src/**/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#f9f7f2",
+      secondary: colors.emerald[600]
+    }
   },
   plugins: [],
 }
