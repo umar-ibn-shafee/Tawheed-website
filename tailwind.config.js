96e0f0ef877e941474b17cf866e4d8a6d2380288
umar-ibn-shafee <md.umar05@gmail.com>
2022-07-09 12:31:32 +0530

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["src/pages/**/*.{js,ts,jsx,tsx}", "src/**/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#f9f7f2",
-      secondary: colors.emerald[600]
+    extend: {
+      colors: {
+        primary: "#f9f7f2",
+        secondary: colors.emerald[600]
+      }
     }
   },
   plugins: [],
