7f159e4053cec51542e7c9c3248aa24a6ad01af6
umar-ibn-shafee <md.umar05@gmail.com>
2022-07-16 23:57:08 +0530

    footer changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 const footer = () => {
     return (
         <div className='relative'>
-            <footer className='h-80 px-12 py-4 bg-black'>
+            <footer className='h-80 px-12 py-4 bg-primary'>
                 <div className='grid grid-row-5 gap-4 h-full'>
                     <div className='row-end-6 row-span-1 p-4 w-max'>
                         <Image src="/logo-horizontal.png" alt="Masjid-e-Tawheed Logo" width={250} height={100} />
                     </div>
                 </div>
-                <div className='absolute inset-x-0 bottom-0 py-2 bg-gradient-to-b from-gray-800 to-black text-white text-xs text-center'>
+                <div className='absolute inset-x-0 bottom-0 py-2 border-t-2 border-primary_hard text-xs text-center'>
                     <p>Copyright © 2022 Masjid-e-Tawheed. All rights reserved.</p>
                 </div>
             </footer>
